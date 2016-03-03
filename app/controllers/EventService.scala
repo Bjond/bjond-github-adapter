d70457283a8bb54523c759276789669b116b737e
bcflynn2 <bcflynn2@gmail.com>
2016-03-03 13:34:47 -0500

    Added support for a push event.
    Had to write the transformer.

--- app/controllers/EventService.scala
+++ app/controllers/EventService.scala
@@ -11,22 +11,35 @@ import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 
 case class IssueComment(assignee: String, repo: String, pull_request: Option[String], user: String)
+case class CodePush(repo: String, ref: String, pusher: String)
 
 class EventService extends Controller {
   
-  implicit val groupConfigurationWrites: Writes[IssueComment] = (
+  implicit val issueCommentWrites: Writes[IssueComment] = (
         (JsPath \ "assignee").write[String] and
         (JsPath \ "repo").write[String] and
         (JsPath \ "pull_request").writeNullable[String] and
         (JsPath \ "user").write[String]
     )(unlift(IssueComment.unapply))
   
-  implicit val groupConfigurationReads: Reads[IssueComment] = (
+  implicit val issueCommentReads: Reads[IssueComment] = (
         (JsPath \ "assignee").read[String] and
         (JsPath \ "repo").read[String] and
         (JsPath \ "pull_request").readNullable[String] and
         (JsPath \ "user").read[String]
     )(IssueComment.apply _)
+    
+  implicit val codePushWrites: Writes[CodePush] = (
+        (JsPath \ "repo").write[String] and
+        (JsPath \ "ref").write[String] and
+        (JsPath \ "pusher").write[String]
+    )(unlift(CodePush.unapply))
+  
+  implicit val codePushReads: Reads[CodePush] = (
+        (JsPath \ "repo").read[String] and
+        (JsPath \ "ref").read[String] and
+        (JsPath \ "pusher").read[String]
+    )(CodePush.apply _)
       
   val prCommentEventTransformer = (
       (JsPath \ "assignee").json.copyFrom((JsPath \ "issue" \ "assignee" \ "login").json.pick) and
@@ -40,6 +53,12 @@ class EventService extends Controller {
       (JsPath \ "user").json.copyFrom((JsPath \ "issue" \ "user" \ "login").json.pick) and 
       (JsPath \ "repo").json.copyFrom((JsPath \ "repository" \ "name").json.pick) 
     ) reduce
+    
+  val pushEventTransformer = (
+      (JsPath \ "repo").json.copyFrom((JsPath \ "head" \ "repo" \ "name").json.pick) and 
+      (JsPath \ "ref").json.copyFrom((JsPath \ "head" \ "ref").json.pick) and 
+      (JsPath \ "pusher").json.copyFrom((JsPath \ "sender" \ "login").json.pick) 
+    ) reduce
   
   def getBodyType(body: Option[JsValue], eventType: String): JsValue = {
     {
@@ -47,9 +66,8 @@ class EventService extends Controller {
       val json = filteredEventType match {
         case "pull_request_review_comment" => body.get.transform(commentEventTransformer).get
         case "issue_comment" => body.get.transform(prCommentEventTransformer).get
+        case "push" => body.get.transform(pushEventTransformer).get
       }
-      //val test = body.get.transform(commentEventTransformer)
-      //val json = body.get.transform(commentEventTransformer).get
       setEvent(json, filteredEventType)
     }
   }
