27305f75d73703a5772d942c84101899d6993858
bcflynn2 <bcflynn2@gmail.com>
2016-03-03 13:18:01 -0500

    Added a conditional transformation.
    A little uglier than I wanted, but functioning.

--- app/controllers/EventService.scala
+++ app/controllers/EventService.scala
@@ -3,40 +3,46 @@ package controllers
 import play.api.mvc._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
+import play.api.data.validation.ValidationError
 import play.api.libs.json.Reads._
 import play.api.libs.ws._
 import scala.concurrent.Future
 import play.api.Play.current
 import scala.concurrent.ExecutionContext.Implicits.global
 
-case class IssueComment(assignee: String, repo: String, pull_request: String, user: String)
+case class IssueComment(assignee: String, repo: String, pull_request: Option[String], user: String)
 
 class EventService extends Controller {
   
   implicit val groupConfigurationWrites: Writes[IssueComment] = (
         (JsPath \ "assignee").write[String] and
         (JsPath \ "repo").write[String] and
-        (JsPath \ "pull_request").write[String] and
+        (JsPath \ "pull_request").writeNullable[String] and
         (JsPath \ "user").write[String]
     )(unlift(IssueComment.unapply))
   
   implicit val groupConfigurationReads: Reads[IssueComment] = (
         (JsPath \ "assignee").read[String] and
         (JsPath \ "repo").read[String] and
-        (JsPath \ "pull_request").read[String] and
+        (JsPath \ "pull_request").readNullable[String] and
         (JsPath \ "user").read[String]
     )(IssueComment.apply _)
+      
+  val prCommentEventTransformer = (
+      (JsPath \ "assignee").json.copyFrom((JsPath \ "issue" \ "assignee" \ "login").json.pick) and
+      (JsPath \ "user").json.copyFrom((JsPath \ "issue" \ "user" \ "login").json.pick) and 
+      (JsPath \ "repo").json.copyFrom((JsPath \ "repository" \ "name").json.pick) and
+      (JsPath \ "pull_request").json.copyFrom((JsPath \ "issue" \ "number").json.pick)
+    ) reduce
     
-  
   val commentEventTransformer = (
-      (JsPath \ "assignee").json.copyFrom((JsPath \ "issue" \ "assignee" \ "login").json.pick.filter(nullFilter)) and
-      (JsPath \ "user").json.copyFrom((JsPath \ "issue" \ "user" \ "login").json.pick.filter(nullFilter)) and 
-      (JsPath \ "repo").json.copyFrom((JsPath \ "repository" \ "name").json.pick.filter(nullFilter)) and
-      (JsPath \ "pull_request").json.copyFrom((JsPath \ "issue" \ "number").json.pick.filter(nullFilter))
+      (JsPath \ "assignee").json.copyFrom((JsPath \ "issue" \ "assignee" \ "login").json.pick) and
+      (JsPath \ "user").json.copyFrom((JsPath \ "issue" \ "user" \ "login").json.pick) and 
+      (JsPath \ "repo").json.copyFrom((JsPath \ "repository" \ "name").json.pick) 
     ) reduce
     
   def nullFilter(value: JsValue) = {
-    value match { 
+    value match {
       case JsNull => true
       case JsString("") => true
       case _ => false
@@ -45,22 +51,31 @@ class EventService extends Controller {
   
   def getBodyType(body: Option[JsValue], eventType: String): JsValue = {
     {
-      val test = body.get.transform(commentEventTransformer)
-      val json = body.get.transform(commentEventTransformer).get
-      setEvent(json, eventType)
+      val filteredEventType: String = getEvent(body.get, eventType)
+      val json = filteredEventType match {
+        case "pull_request_review_comment" => body.get.transform(commentEventTransformer).get
+        case "issue_comment" => body.get.transform(prCommentEventTransformer).get
+      }
+      //val test = body.get.transform(commentEventTransformer)
+      //val json = body.get.transform(commentEventTransformer).get
+      setEvent(json, filteredEventType)
     }
   }
   
   def setEvent(json: JsValue, eventType: String): JsValue = {
-    val event: String = {
-      if (eventType.equals("issue_comment") && !(json \ "pull_request").equals(null)) {
+    val event: String = getEvent(json, eventType)
+    json.as[JsObject] + ("event" -> Json.toJson(event))
+  }
+  
+  def getEvent(json: JsValue, eventType: String): String = {
+    {
+      if (eventType.equals("issue_comment") && !(json \ "issue" \ "pull_request").equals(null)) {
         "pull_request_review_comment"
       }
       else {
         eventType
       }
     }
-    json.as[JsObject] + ("event" -> Json.toJson(event))
   }
   
   def fireEvent(url: String, json: Option[JsValue], event: String): Future[WSResponse] = {
